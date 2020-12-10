811b59760516f78e32d685bd028328e59a4d75d6
Eneki <joanreneki@gmail.com>
2020-12-10 22:19:49 +0300

    Kata day 10

---
+++ kata-10.js
@@ -0,0 +1,89 @@
+// For building the encrypted string:
+//     Take every 2 n d char from the string, then the other chars, that are not every 2 n d char, and concat them as new String.
+// Do this n times!
+
+//     Examples:
+
+//     "This is a test!", 1 - > "hsi  etTi sats!"
+// "This is a test!", 2 - > "hsi  etTi sats!" - > "s eT ashi tist!"
+// Write two methods:
+
+//     function encrypt(text, n)
+
+// function decrypt(encryptedText, n)
+// For both methods:
+//     If the input - string is null or empty
+// return exactly this value!
+//     If n is <= 0 then
+// return the input text.
+function encrypt(text, n) {
+    let count = 1,
+        i, newStringPartOne = '',
+        newStringPartTwo = '';
+    if (n <= 0 || text == (null || "")) {
+        finalString = text
+        return finalString
+    }
+
+    while (count <= n) {
+        for (i = 1; i < text.length; i = i + 2) {
+            newStringPartOne += text[i]
+        }
+        for (i = 0; i < text.length; i = i + 2) {
+            newStringPartTwo += text[i]
+        }
+
+        finalString = newStringPartOne + newStringPartTwo
+        text = finalString
+
+        newStringPartOne = newStringPartTwo = ''
+
+        count++
+    }
+
+    return finalString
+}
+
+function decrypt(encryptedText, n) {
+    let decryptedString = '',
+        countSecondHalf = 0,
+        countFirstHalf = 0,
+        count = 1;
+
+    if (n <= 0 || encryptedText == (null || "")) {
+        finalString = encryptedText;
+        return finalString;
+    }
+
+    // Split into half - if odd, more are on the right
+    // First for push from second for
+
+    lengthOfHalfEncryptedString = Math.trunc(encryptedText.length / 2);
+    firstHalf = encryptedText.substr(0, lengthOfHalfEncryptedString);
+    secondHalf = encryptedText.substr(lengthOfHalfEncryptedString, encryptedText.length - 1);
+
+    //   console.log(firstHalf)
+    //   console.log(secondHalf)
+
+    while (count <= n) {
+        for (i = 0; i < encryptedText.length; i++) {
+            if (i == 0 || i % 2 == 0) {
+                decryptedString += secondHalf[countSecondHalf];
+                countSecondHalf++;
+            } else {
+                decryptedString += firstHalf[countFirstHalf];
+                countFirstHalf++;
+            }
+        }
+
+        count++;
+        if (count <= n) {
+            encryptedText = decryptedString;
+            decryptedString = ''
+        } else {
+            finalString = decryptedString;
+        }
+    }
+
+    return finalString;
+}
